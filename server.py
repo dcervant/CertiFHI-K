9481c882d4aed3e9d9042be770b6335ef0eb3b4d
Dherly Cervantes <dherly.cervantes@rimac.com.pe>
2026-03-07 00:03:11 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(title="FHIR Quiz Static")
+
+# Sirve index.html, app.js, questions.json, etc.
+app.mount("/", StaticFiles(directory="web", html=True), name="web")
